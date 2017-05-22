207ffd4dc1be1761130f6d0e367885b8cfc1e908
gloutch <ismael.herve@yahoo.com>
2017-05-22 16:42:11 +0200

    new formula on pendulum

--- pendulum.py
+++ pendulum.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import empty, arange, array, cos, sin
 from cauchy import meth_n_step, step_rk4
 
 
@@ -47,74 +47,88 @@ def frequencies(Theta, g, l):
         h = 0.3
         N = 50 # probably it's enough
         
-        x = np.arange(t0, t0 + N*h, h)
-        y = np.empty(N)
+        x = arange(t0, t0 + N*h, h)
+        y = empty(N)
         # theta'' + (g/l)sin(theta) = 0
-        F = lambda t, Y: np.array([Y[1], -(g/l) * np.sin(Y[0])])
+        F = lambda t, Y: array([Y[1], -(g/l) * sin(Y[0])])
 
         n = len(Theta)
-        freq = np.empty(n)
+        freq = empty(n)
         for i in range(n):
 
-                Y0 = np.array([Theta[i], 0])
+                Y0 = array([Theta[i], 0])
                 Y = meth_n_step(Y0, t0, N, h, F, step_rk4)
                 for j in range(N) :
                         y[j] = Y[j][0]
-
                 freq[i] = 1 / period(x, y)
 
         return freq
 
 
 # Y = [th1, th1', th2, th2']
-def step_double_pendulum(Y, g, l):
-        gl = g / l
-        cos_dth = np.cos(Y[0] - Y[2])
-        sin_dth = np.sin(Y[0] - Y[2])
-        sin_th1 = np.sin(Y[0])
-        sin_th2 = np.sin(Y[2])
-        alpha = 1 / (1 - ((cos_dth * cos_dth) / 2))
+def step_double_pendulum(Y, g, l, m):
+        th1 = Y[0]
+        th11 = Y[1]
+        th2 = Y[2]
+        th22 = Y[3]
+        alpha = 1 / (l * (3 * m - m * cos(2 * th1 - 2 * th2)))
         
-        Y1 = alpha * (Y[1] * Y[1] * sin_dth * cos_dth / 2
-                      + gl * sin_th2 * cos_dth
-                      - Y[3] * Y[3] * sin_dth / 2
-                      - gl * sin_th1)
-        Y3 = alpha * (Y[3] * Y[3] * sin_dth * cos_dth / 2
-                      + gl * sin_th1 * cos_dth
-                      + Y[1] * Y[1] * sin_dth
-                      - gl * sin_th2)
+        th111 = alpha * (-3 * g * m * sin(th1) - g * m * sin(th1 - 2 * th2)
+        - 2 * sin(th1 - th2) * m * (th22 * th22 * l + th11 * th11 * cos(th1 - th2)))
 
-        return np.array([Y[1], Y1, Y[3], Y3]) # [th1', th1'', th2', th2'']
+        th222 = alpha * (2 * sin(th1 - th2) * (th11 * th11 * l * 2 * m + 2 * g * m * cos(th1)
+        + th22 * th22 * l * m * cos(th1 - th2)))
+
+        return array([th11, th111, th22, th222]) # [th1', th1'', th2', th2'']
 
 
-def pendulum_path(th1, th2, g, l, N, h):
-        Y0 = np.array([th1, 0, th2, 0])
-        F = lambda t, Y: step_double_pendulum(Y, g, l)
+def pendulum_path(th1, th2, g, l, m, N, h):
+        Y0 = array([th1, 0, th2, 0])
+        F = lambda t, Y: step_double_pendulum(Y, g, l, m)
         sol = meth_n_step(Y0, 0, N, h, F, step_rk4)
 
-        n = len(sol)
-        x2 = np.empty(n)
-        for i in range(n): x2[i] = np.sin(sol[i][0]) + np.sin(sol[i][2])
-        y2 = np.empty(n)
-        for i in range(n): y2[i] = -np.cos(sol[i][0]) - np.cos(sol[i][2])
-
+        x2 = empty(N)
+        y2 = empty(N)
+        for i in range(N):
+            x2[i] =  sin(sol[i][0]) + sin(sol[i][2])
+            y2[i] = -cos(sol[i][0]) - cos(sol[i][2])
+            
         return x2, y2
 
 
-def flip_over_ratio(th1, th2, g, l):
-        N = 100
-        h = 0.5
-        Y0 = np.array([th1, 0, th2, 0])
-        F = lambda t, Y: step_double_pendulum(Y, g, l)
+def pendulum_all_info(th1, th2, g, l, m, N, h):
+        Y0 = array([th1, 0, th2, 0])
+        F = lambda t, Y: step_double_pendulum(Y, g, l, m)
         sol = meth_n_step(Y0, 0, N, h, F, step_rk4)
 
-        pi = 3.14
-        # theta2 = sol[:][2]
-        for i in range(1, N):
-                if (sol[i - 1][2] <= pi):
-                        if (sol[i][2] >= pi):
-                                return i / N
-                else:
-                        if (sol[i][2] <= pi):
-                                return i / N
+        t1 = empty(N)
+        t2 = empty(N)
+        x1 = empty(N)
+        x2 = empty(N)
+        y1 = empty(N)
+        y2 = empty(N)
+        for i in range(N):
+            t1[i] = sol[i][0]
+            t2[i] = sol[i][2]
+            
+            x1[i] = sin(t1[i])
+            x2[i] = x1[i] + sin(t2[i])
+            
+            y1[i] = -cos(t1[i])
+            y2[i] = y1[i] - cos(t2[i])
+            
+        return t1, t2, x1, y1, x2, y2
+
+
+def flip_over_ratio(th1, th2, g, l, m):
+        N = 200
+        h = 0.5
+        Y0 = array([th1, 0, th2, 0])
+        F = lambda t, Y: step_double_pendulum(Y, g, l, m)
+        sol = meth_n_step(Y0, 0, N, h, F, step_rk4)
+
+        for i in range(N):
+            if (3 * cos(sol[i][0]) + cos(sol[i][2]) > 2):
+                return i / N
+            
         return 1
--- pendulum_test.py
+++ pendulum_test.py
@@ -1,8 +1,7 @@
 from pendulum import *
+from matplotlib import animation
+from matplotlib import pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
 
 def period_convergency():
         expected = 2 * np.pi
@@ -44,19 +43,42 @@ def draw_freqencies():
 # draw_freqencies()
 
 
+# fig = plt.figure()
+
+# ax = plt.axes(xlim=(0, 2), ylim=(0, 100))
+
+# N = 4
+# lines = [plt.plot([], [])[0] for _ in range(N)]
+
+# def init():    
+#     for line in lines:
+#         line.set_data([], [])
+#     return lines
+
+# def animate(i):
+#     for j,line in enumerate(lines):
+#         line.set_data([0, 2], [10 * j,i])
+#     return lines
+
+# anim = animation.FuncAnimation(fig, animate, init_func=init,
+#                                frames=100, interval=20, blit=True)
+
+# plt.show()
+
+
 def draw_pendulum():
         N = 300
         h = 0.1
+        m = 1
         g = 9.81
         l = 5
 
-        # TODO, fix limit of frame
-
         plt.subplot(1, 3, 1)
+        #plt.axes(xlim=(-2, 2), ylim=(-2, 2))
         th1 = 2
         th2 = 1.5
         plt.title("th1: " + str(th1) + "rad , th2: " + str(th2) + "rad")
-        x, y = pendulum_path(th1, th2, g, l, N, h)
+        x, y = pendulum_path(th1, th2, g, l, m, N, h)
         plt.axis("equal")
         plt.plot(x, y)
         linex = [0,  np.sin(th1),  np.sin(th1) + np.sin(th2)]
@@ -64,10 +86,11 @@ def draw_pendulum():
         plt.plot(linex, liney, "o-")
         
         plt.subplot(1, 3, 2)
+        #plt.axes(xlim=(-2, 2), ylim=(-2, 2))
         th1 = th1 + 0.02
         th2 = th2
         plt.title("th1: " + str(th1) + "rad , th2: " + str(th2) + "rad")
-        x, y = pendulum_path(th1, th2, g, l, N, h)
+        x, y = pendulum_path(th1, th2, g, l, m, N, h)
         plt.axis("equal")
         plt.plot(x, y)
         linex = [0,  np.sin(th1),  np.sin(th1) + np.sin(th2)]
@@ -75,10 +98,11 @@ def draw_pendulum():
         plt.plot(linex, liney, "o-")
 
         plt.subplot(1, 3, 3)
+        #plt.axes(xlim=(-2, 2), ylim=(-2, 2))
         th1 = th1 + 0.02
         th2 = th2
         plt.title("th1: " + str(th1) + "rad , th2: " + str(th2) + "rad")
-        x, y = pendulum_path(th1, th2, g, l, N, h)
+        x, y = pendulum_path(th1, th2, g, l, m, N, h)
         plt.axis("equal")
         plt.plot(x, y)
         linex = [0,  np.sin(th1),  np.sin(th1) + np.sin(th2)]
@@ -93,17 +117,18 @@ def draw_pendulum():
 def draw_flip_over(N):
         g = 9.81
         l = 5
+        m = 1
         angle = lambda i: -3 + (i * 6 / N)
         
         M = np.empty((N, N))
         for i in range(N):
                 for j in range(N):
-                        M[i][j] = flip_over_ratio(angle(i), angle(j), g, l)
+                        M[i][j] = flip_over_ratio(angle(j), angle(i), g, l, m)
 
         fig = plt.figure(5)
         ax = plt.subplot(111)
-        im = ax.imshow(M, cmap=plt.get_cmap('hot'))
+        im = ax.imshow(M, cmap=plt.get_cmap("YlGn"))
         fig.colorbar(im)
         plt.show()
 
-draw_flip_over(300)
+draw_flip_over(20)
